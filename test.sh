ed7be62138a238c624eee39f5801e7534649cba9
manrajag <manrajag@users.noreply.github.com>
2014-12-05 17:48:39 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget https://github.com/manrajag/GSB/raw/master/mait.tar.gz --no-check-certificate -O /tmp/mait.tgz
+
+wget https://github.com/manrajag/GSB/raw/master/mait_service --no-check-certificate -O /tmp/mait.sh
+
+
